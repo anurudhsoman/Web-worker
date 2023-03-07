18a05ee6817982d04175f654be07f84e3d5d0397
anurudhsoman <95540218+anurudhsoman@users.noreply.github.com>
2023-03-07 11:33:53 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,6 +1,4 @@
-const worker = new worker('timer-worker.js')
-  const start = performance.now()
+
+
   console.log("js file from git")
-  setTimeout(() => {
-    console.log(performance.now() - start)
-  }, 1000 * 60 * 10) 
+
